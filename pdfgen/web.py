21c1beb7f3a49032d6797a79cf59647357334d5d
Dmitry Romanov <me@wutiarn.ru>
2017-02-14 17:32:30 +0300

    Page count limit

--- pdfgen/web.py
+++ pdfgen/web.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import send_file
+from flask import send_file, abort
 from tempfile import NamedTemporaryFile
 from pdfgen import pdf
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<int:page_count>")
 def root(page_count=1):
+    if page_count > 100:
+        abort(400, "Too many pages requested")
+
     file = NamedTemporaryFile()
     pdf.make_pdf(page_count, file)
     file.seek(0)
